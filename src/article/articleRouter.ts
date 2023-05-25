8dfa99060809b4191d254721fcb8d9145637a668
vlasa <vlasq91@gmail.com>
2023-05-25 07:39:02 +0300

    Split the validation middleware from the controller into its own file

--- src/article/articleRouter.ts
+++ src/article/articleRouter.ts
@@ -1,10 +1,24 @@
 import { Router } from 'express';
-import { getArticles, postArticle } from './articleController';
+import { getArticles, postArticle, updateArticle } from './articleController';
 import passport from 'passport';
+import { confirmValidation, validateGetArticles, validateTransformationOfArticle } from './validationMiddleware';
 
 const articleRouter = Router();
 
-articleRouter.post('/', passport.authenticate('jwt', { session: false }), postArticle);
-articleRouter.get('/', getArticles);
+articleRouter.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  validateTransformationOfArticle,
+  confirmValidation,
+  postArticle
+);
+articleRouter.put(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  validateTransformationOfArticle,
+  confirmValidation,
+  updateArticle
+);
+articleRouter.get('/', validateGetArticles, confirmValidation, getArticles);
 
 export default articleRouter;
---
+++ src/article/validationMiddleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express';
+import { body, query, validationResult } from 'express-validator';
+
+export const validateTransformationOfArticle = [
+  body('title', 'Title must be at least 3 characters long.').trim().isLength({ min: 3 }).escape(),
+  body('createdAt').trim().exists().escape(),
+  body('content', 'The article must be at least 100 characters long.').trim().isLength({ min: 100 }).escape(),
+  body('published').trim().exists().escape()
+];
+
+export const validateGetArticles = [
+  query('page').optional().trim().escape().isInt().withMessage('The page must be a integer.'),
+  query('pageSize').optional().trim().escape().isInt().withMessage('The page must be a integer.'),
+  query('sort').optional().trim().isIn(['asc', 'desc']).withMessage('Sort must be "asc" or "desc".').escape(),
+  query('before').optional().trim().isDate().escape(),
+  query('after').optional().trim().isDate().escape()
+];
+
+export const confirmValidation = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.send({ errors: errors.array() });
+  next();
+};
