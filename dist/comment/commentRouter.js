44fde69efa2a7d1ff3a3178cecaaa9b8239315bb
vlasa <vlasq91@gmail.com>
2023-06-03 09:17:34 +0300

    Resolved import .ts in dist folder

--- dist/comment/commentRouter.js
+++ dist/comment/commentRouter.js
@@ -4,11 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const commentController_ts_1 = require("./commentController.ts");
-const validationMiddleware_ts_1 = require("./validationMiddleware.ts");
-const confirmValidation_ts_1 = __importDefault(require("../middleware/confirmValidation.ts"));
+const commentController_1 = require("./commentController");
+const validationMiddleware_1 = require("./validationMiddleware");
+const confirmValidation_1 = __importDefault(require("../middleware/confirmValidation"));
 const commentRouter = (0, express_1.Router)();
-commentRouter.post('/:articleId/comments', validationMiddleware_ts_1.validateCommentBody, confirmValidation_ts_1.default, commentController_ts_1.postComment);
-commentRouter.put('/:articleId/comments/:commentId', validationMiddleware_ts_1.validateCommentBody, confirmValidation_ts_1.default, commentController_ts_1.updateComment);
-commentRouter.delete('/:articleId/comments/:commentId', validationMiddleware_ts_1.validateCommentToDelete, confirmValidation_ts_1.default, commentController_ts_1.deleteComment);
+commentRouter.post('/:articleId/comments', validationMiddleware_1.validateCommentBody, confirmValidation_1.default, commentController_1.postComment);
+commentRouter.put('/:articleId/comments/:commentId', validationMiddleware_1.validateCommentBody, confirmValidation_1.default, commentController_1.updateComment);
+commentRouter.delete('/:articleId/comments/:commentId', validationMiddleware_1.validateCommentToDelete, confirmValidation_1.default, commentController_1.deleteComment);
 exports.default = commentRouter;
