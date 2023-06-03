8ec3b7beee9d6842c04799b1405d6be55a4d9fbc
vlasa <vlasq91@gmail.com>
2023-06-03 09:23:18 +0300

    Added origin to cors

--- dist/server.js
+++ dist/server.js
@@ -21,7 +21,7 @@ const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8000;
 // Middleware
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({ origin: 'http://localhost:5173' }));
 app.use((0, compression_1.default)());
 app.use((0, helmet_1.default)());
 app.use(express_1.default.json());
